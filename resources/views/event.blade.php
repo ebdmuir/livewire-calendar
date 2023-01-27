f859a2cd423feed2f36cae9cfa9ae8da491d475b
Eric Muir <eric.muir@xdesign.com>
2023-01-27 10:42:07 +0000

    Add colors

--- resources/views/event.blade.php
+++ resources/views/event.blade.php
@@ -2,7 +2,8 @@
     @if($eventClickEnabled)
         wire:click.stop="onEventClick('{{ $event['id']  }}')"
     @endif
-    class="bg-white rounded-lg border py-2 px-3 shadow-md cursor-pointer">
+    class="{{ $event['color'] == null ? 'bg-white' : null }} {{ $event['textColor'] == null ? 'text-white' : 'text-' . $event['textColor'] }} rounded-lg border py-2 px-3 shadow-md cursor-pointer"
+    style="background-color: {{ $event['color'] }}">
 
     <p class="text-sm font-medium">
         {{ $event['title'] }}
