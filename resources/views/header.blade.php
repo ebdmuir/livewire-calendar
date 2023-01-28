54659ee15e8d1878e838d96d486ce8f33b5ca63a
Eric Muir <eric.muir@xdesign.com>
2023-01-28 16:40:18 +0000

    Fix view button label

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -3,7 +3,7 @@
     <time datetime="2022-01">{{ $startsAt->englishMonth }} {{ $startsAt->year }}</time>
   </h1>
   <div class="flex items-center">
-    <div class="flex items-center rounded-md shadow-sm md:items-stretch">
+    <div class="flex items-center rounded-md shadow-sm md:items-stretch pr-2">
       <button type="button" wire:click="decrement" class="flex items-center justify-center rounded-l-md border border-r-0 border-gray-300 bg-white py-2 pl-3 pr-4 text-gray-400 hover:text-gray-500 focus:relative md:w-9 md:px-2 md:hover:bg-gray-50">
         <span class="sr-only">Previous month</span>
         <!-- Heroicon name: mini/chevron-left -->
@@ -11,7 +11,7 @@
           <path fill-rule="evenodd" d="M12.79 5.23a.75.75 0 01-.02 1.06L8.832 10l3.938 3.71a.75.75 0 11-1.04 1.08l-4.5-4.25a.75.75 0 010-1.08l4.5-4.25a.75.75 0 011.06.02z" clip-rule="evenodd" />
         </svg>
       </button>
-      <button type="button" class="hidden border-t border-b border-gray-300 bg-white px-3.5 text-sm font-medium text-gray-700 hover:bg-gray-50 hover:text-gray-900 focus:relative md:block">Today</button>
+      <button type="button" class="hidden border-t border-b border-gray-300 bg-white py-2 px-3.5 text-sm font-medium text-gray-700 hover:bg-gray-50 hover:text-gray-900 focus:relative md:block">Today</button>
       <span class="relative -mx-px h-5 w-px bg-gray-300 md:hidden"></span>
       <button type="button" wire:click="increment" class="flex items-center justify-center rounded-r-md border border-l-0 border-gray-300 bg-white py-2 pl-4 pr-3 text-gray-400 hover:text-gray-500 focus:relative md:w-9 md:px-2 md:hover:bg-gray-50">
         <span class="sr-only">Next month</span>
@@ -24,7 +24,7 @@
     <div class="hidden md:ml-4 md:flex md:items-center">
       <div class="relative">
         <button type="button" wire:click="toggleMenu" class="flex items-center rounded-md border border-gray-300 bg-white py-2 pl-3 pr-2 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50" id="menu-button" aria-expanded="false" aria-haspopup="true">
-          Week view
+          {{ ucfirst(trans($increment)) }} view
           <!-- Heroicon name: mini/chevron-down -->
           <svg class="ml-2 h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
             <path fill-rule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" clip-rule="evenodd" />
@@ -47,7 +47,7 @@
             {{-- <a href="#" class="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-0">Day view</a> --}}
             <button wire:click="setMode('week')" class="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-1">Week view</button>
             <button wire:click="setMode('month')" class="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-2">Month view</button>
-            <button wire:click="setMode('year')" class="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-3">Year view</button>
+            {{-- <button wire:click="setMode('year')" class="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-3">Year view</button> --}}
           </div>
         </div>
       </div>
@@ -55,13 +55,13 @@
       <button type="button" class="ml-6 rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">Add event</button>
     </div>
     <div class="relative ml-6 md:hidden">
-      <button type="button" class="-mx-2 flex items-center rounded-full border border-transparent p-2 text-gray-400 hover:text-gray-500" id="menu-0-button" aria-expanded="false" aria-haspopup="true">
+      {{-- <button type="button" class="-mx-2 flex items-center rounded-full border border-transparent p-2 text-gray-400 hover:text-gray-500" id="menu-0-button" aria-expanded="false" aria-haspopup="true">
         <span class="sr-only">Open menu</span>
         <!-- Heroicon name: mini/ellipsis-horizontal -->
         <svg class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
           <path d="M3 10a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0zM8.5 10a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0zM15.5 8.5a1.5 1.5 0 100 3 1.5 1.5 0 000-3z" />
         </svg>
-      </button>
+      </button> --}}
 
       <!--
         Dropdown menu, show/hide based on menu state.
@@ -73,7 +73,7 @@
           From: "transform opacity-100 scale-100"
           To: "transform opacity-0 scale-95"
       -->
-      <div class="absolute right-0 z-10 mt-3 w-36 origin-top-right divide-y divide-gray-100 overflow-hidden rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="menu-0-button" tabindex="-1">
+      {{-- <div class="absolute right-0 z-10 mt-3 w-36 origin-top-right divide-y divide-gray-100 overflow-hidden rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="menu-0-button" tabindex="-1">
         <div class="py-1" role="none">
           <!-- Active: "bg-gray-100 text-gray-900", Not Active: "text-gray-700" -->
           <a href="#" class="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-0-item-0">Create event</a>
@@ -87,7 +87,7 @@
           <a href="#" class="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-0-item-4">Month view</a>
           <a href="#" class="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-0-item-5">Year view</a>
         </div>
-      </div>
+      </div> --}}
     </div>
   </div>
 </header>
