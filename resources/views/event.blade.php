95e8e0206d857fbca671a95fbf0e2c64c089abf0
Eric Muir <eric.muir@xdesign.com>
2023-01-28 16:39:52 +0000

    Add event time

--- resources/views/event.blade.php
+++ resources/views/event.blade.php
@@ -1,7 +1,4 @@
-<div
-    @if($eventClickEnabled)
-        wire:click.stop="onEventClick('{{ $event['id']  }}')"
-    @endif
+<div @if ($eventClickEnabled) wire:click.stop="onEventClick('{{ $event['id'] }}')" @endif
     class="{{ $event['color'] == null ? 'bg-white' : null }} {{ $event['textColor'] == null ? 'text-white' : 'text-' . $event['textColor'] }} rounded-lg border py-2 px-3 shadow-md cursor-pointer"
     style="background-color: {{ $event['color'] }}">
 
@@ -9,6 +6,8 @@
         {{ $event['title'] }}
     </p>
     <p class="mt-2 text-xs">
-        {{ $event['description'] ?? 'No description' }}
+    <div class="flex items-start space-x-3 text-sm">
+        <time datetime="2022-01-10T17:00">{{ $event['date']->format('h:i') }}</time>
+    </div>
     </p>
 </div>
